f84713c6fbcf2f1c116d8161a0fcd2d26f36f59e
Dipak-Saletify <dipak@saletify.com>
2023-11-01 18:17:55 +0530

    added enquire migration table

--- resources/views/email-body/email-enquiry2.blade.php
+++ resources/views/email-body/email-enquiry2.blade.php
@@ -2,45 +2,35 @@
 
 <html>
 
-
-
 <head>
-
     <title>Customer Mail </title>
-
 </head>
-
-
 
 <body>
 
-
-
     <p> Dear {{ $fromName }},</p>
 
+   <p>Thank you for expressing your interest in our Investment Banking Operation Program. Your curiosity about advancing your knowledge in investment banking operations is highly appreciated.</p>
+   <p>Now, take a moment to relax, as our team is eager to connect with you. Within the next 4 hours, one of our dedicated representatives will reach out to discuss the program, answer any queries you may have, and guide you through the enrollment process.</p>
+   <p>We appreciate your interest and are excited to embark on this educational journey with you. Should you have any immediate questions or require assistance, please feel free to contact us directly at <a href="mailto:admissions@datagami.in">admissions@datagami.in</a></p>
+     
+     <p>Thank you again for considering FinLearn as your partner in mastering the intricacies of investment banking operations.</p>
 
+     <p>Relax, and we'll be in touch soon!</p>
 
-    <p>Thanks for your enquiry submission.! </p>
-    <p>Below are the details submitted from you. </p>
-    <strong>Name:</strong> {{ $fromName }}<br>
-    <strong>Email:</strong> {{ $fromEmail }}<br>
-    <strong>Phone:</strong> {{ $phone }}<br>
-     <strong>City:</strong> {{ $city }}<br>
-     <strong>Experience:</strong> {{ $exp_yrs }} Years - {{ $months }} Months<br>
-    <p>Our Team will call you with in 4 working hours.</p>
-    <p> In case of query, please revert. </p>
-
-    Rgds,<br>
-    Finlearn <br>
+    
+    Warm Regards,<br>
+    Team FinLearn<br>
     Phone: +91 9833443014<br>
-    Email: admissions@datagami.in
-
-
-
-
+Email: admissions@datagami.in
+    
+    <!--    <p>Below are the details submitted from you. </p>-->
+    <!--<strong>Name:</strong> {{ $fromName }}<br>-->
+    <!--<strong>Email:</strong> {{ $fromEmail }}<br>-->
+    <!--<strong>Phone:</strong> {{ $phone }}<br>-->
+    <!--<strong>City:</strong> {{ $city }}<br>-->
+    <!--<strong>Experience:</strong> {{ $exp_yrs }} Years - {{ $months }} Months<br>-->
+    
 
 </body>
-
-
-
 </html>
