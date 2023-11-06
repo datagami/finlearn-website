94780effe2ec199c09397d01eac1c2a983a62239
Dipak-Saletify <dipak@saletify.com>
2023-11-06 11:37:22 +0530

    added status field to enquire table

--- database/migrations/2023_07_07_061639_create_enquires_table.php
+++ database/migrations/2023_07_07_061639_create_enquires_table.php
@@ -21,6 +21,7 @@ return new class extends Migration
             $table->string('city');
             $table->string('exp_yrs');
             $table->string('months');
+            $table->integer('status')->default(1);
             $table->timestamps();
         });
     }
